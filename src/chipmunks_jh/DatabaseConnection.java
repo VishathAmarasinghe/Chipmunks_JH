efb6b47428cef9cc20b7d1b5b5de8db797577eab
Thamidu-Premaratna <115412689+Thamidu-Premaratna@users.noreply.github.com>
2023-07-22 12:21:10 +0530

    major changes in Database Connection

--- src/chipmunks_jh/DatabaseConnection.java
+++ src/chipmunks_jh/DatabaseConnection.java
@@ -10,24 +10,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DatabaseConnection {
-
     private Connection con;
 
-    public DatabaseConnection() {
-        try {
-            setCon(DriverManager.getConnection("jdbc:mysql://localhost:3306/LIPS", "root", ""));
-        } catch (SQLException ex) {
-            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public DatabaseConnection()
+    {
+    }
+    public void databaseConnection() throws ClassNotFoundException, SQLException
+    {
+        setCon(DriverManager.getConnection("jdbc:mysql://localhost:3306/LIPS", "root", ""));
+    }
+    public void closeDBcon() throws SQLException
+    {
+        getCon().close();
     }
 
-    public void closeDBcon() {
-        try {
-            getCon().close();
-        } catch (SQLException ex) {
-            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 
     public Connection getCon() {
         return con;
@@ -36,10 +32,9 @@ public class DatabaseConnection {
     public void setCon(Connection con) {
         this.con = con;
     }
-
-    public void InsertBook(String ISBN, String Title, String CatID) throws SQLException {
-        String statement = "INSERT INTO `Book`(`ISBN`, `TItle`, `CatID`) VALUES (?,?,?)";
-        PreparedStatement ps = getCon().prepareStatement(statement);
+    public void InsertBook(String ISBN,String Title,String CatID) throws SQLException{
+        String statement="INSERT INTO `Book`(`ISBN`, `TItle`, `CatID`) VALUES (?,?,?)";
+        PreparedStatement ps= getCon().prepareStatement(statement);
         try {
             ps = getCon().prepareStatement(statement);
         } catch (SQLException ex) {
@@ -50,15 +45,14 @@ public class DatabaseConnection {
         } catch (SQLException ex) {
             Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
-        ps.setString(1, Title);
-        ps.setString(1, CatID);
+        ps.setString(2, Title);
+        ps.setString(3, CatID);
         ps.executeUpdate();
-        closeDBcon();
+        
     }
-
-    public void InsertCategory(String Category) throws SQLException {
-        String statement = "INSERT INTO `Category`(`CatName`) VALUES (?)";
-        PreparedStatement ps = getCon().prepareStatement(statement);
+    public void InsertCategory(String Category) throws SQLException{
+        String statement="INSERT INTO `Category`(`CatName`) VALUES (?)";
+        PreparedStatement ps= getCon().prepareStatement(statement);
         try {
             ps = getCon().prepareStatement(statement);
         } catch (SQLException ex) {
@@ -70,22 +64,38 @@ public class DatabaseConnection {
             Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
         ps.executeUpdate();
-        closeDBcon();
-
+        
     }
-
-    public int FindCategory(String CatName) throws ClassNotFoundException, SQLException {
-        Statement statement = getCon().createStatement();
-        String stmt = "SELECT `CatName` FROM `Category`";
-        ResultSet rst = statement.executeQuery(stmt);
-        if (rst.next()) {
-            if (rst.getString(1) == CatName) {
-                return 1;
-            } else {
-                return 0;
-            }
+    public int FindCategory(String CatName) throws ClassNotFoundException, SQLException{
+        databaseConnection();
+        Statement statement=getCon().createStatement();
+        String stmt="SELECT `CatName` FROM `Category`";
+        ResultSet rst=statement.executeQuery(stmt);
+        if(rst.next()){
+           if(rst.getString(1) == null ? CatName == null : rst.getString(1).equals(CatName)){
+               return 1;
+           }
+           else{
+           return 0;}
         }
         closeDBcon();
         return 0;
+    }
+    public void UpdatetBook(String ISBN,String Title,String CatID) throws SQLException{
+        String statement="UPDATE `Book` SET `ISBN`=?,`TItle`=?,`CatID`=? WHERE ISBN=ISBN;";
+        PreparedStatement ps= getCon().prepareStatement(statement);
+        try {
+            ps = getCon().prepareStatement(statement);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            ps.setString(1, ISBN);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ps.setString(2, Title);
+        ps.setString(3, CatID);
+        ps.executeUpdate();
     }
 }
